73205362497a8645d88eca2ffea0f089007c110a
Nupur <nupursogani3101@gmail.com>
2021-06-05 01:26:32 +0530

    crop two images and swap them with each other

--- ImageProcessing/Task2/CropSwap.py
+++ ImageProcessing/Task2/CropSwap.py
@@ -1,19 +1,38 @@
-# 📌 Take 2 images and combine them to form a single image. For example, collage 
+# 📌 Take 2 images, crop some part of both the images and swap them.
 
 import cv2
 import numpy as np
 
-# Loading 2 images
-p1 = cv2.imread('puzzleL.jpg')
-p2 = cv2.imread('puzzleR.jpg')
+#loading image 1
+photo = cv2.imread('white.jpg')
 
-# displaying images
-cv2.imshow('p1',p1)
-cv2.imshow('p2',p2)
+#loading image 2
+pic = cv2.imread('violet.jpg')
+
+# Displaying both images
+cv2.imshow('pic1',photo)
+cv2.imshow('pic2',pic)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-# combining the images using numpy array horizontal stack function
-cv2.imshow('pic2',np.hstack((p1,p2[0:477,:])))
+#Cropping some part of both images
+x = photo[70:280, 200:450]
+y = pic[50:260, 210:460]
+
+#Displaying cropped portion
+cv2.imshow('crop_pic1',x)
+cv2.imshow('crop_pic2',y)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+photo = cv2.imread('white.jpg')
+pic = cv2.imread('violet.jpg')
+
+#Swapping the images
+photo[70:280, 200:450] = y
+pic[50:260, 210:460] = x
+cv2.imshow('swap_pic1',photo)
+cv2.imshow('swap_pic2',pic)
 cv2.waitKey()
 cv2.destroyAllWindows()
